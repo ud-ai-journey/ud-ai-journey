e51ab9951b988da96842e92e82d6c22695123a5a
ud.ai.journey@gmail.com <ud.ai.journey@gmail.com>
2025-04-19 23:03:22 +0530

    🎯 Completed Day 4: Practised Patterns using for loop | Practiced visual logic & alignment

---
+++ day_4/pattern_1.py
@@ -0,0 +1,4 @@
+# 🌟 Pattern 1: Vertical line of stars
+n = 5
+for i in range(n):
+    print("*")  # Prints one star per line (5 lines total)
---
+++ day_4/pattern_6.py
@@ -0,0 +1,4 @@
+# 🌟 Pattern 6: Inverted right-angled triangle (right-aligned)
+n = 5
+for i in range(n):
+    print(" " * i + "*" * (n - i))  # Increasing space, decreasing stars
