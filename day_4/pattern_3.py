15d3deb7c88c22089134c1a716100fc99402b31d
ud.ai.journey@gmail.com <ud.ai.journey@gmail.com>
2025-04-19 22:56:51 +0530

    🎯 Completed Day 4: Practised Patterns using for loop | Practiced visual logic & alignment

---
+++ day_4/pattern_3.py
@@ -0,0 +1,4 @@
+# 🌟 Pattern 3: Right-angled triangle (increasing stars)
+n = 5
+for i in range(n):
+    print("*" * i)  # Each row has i stars (starts from 0)
---
+++ day_4/pattern_4.py
@@ -0,0 +1,4 @@
+# 🌟 Pattern 4: Inverted right-angled triangle (decreasing stars)
+n = 5
+for i in range(n):
+    print("*" * (n - i))  # Starts with 5 stars, reduces by 1 every row
